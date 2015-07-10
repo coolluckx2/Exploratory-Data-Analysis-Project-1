09f27571670f2b71be8eef8e8c75a9a4238230c5
coolluckx2 <ggozzasnu@snu.ac.kr>
2015-07-11 00:45:03 +0900

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -17,10 +17,10 @@ png("plot4.png", width=480, height=480)
 par(mfrow=c(2,2))
 
 plot(subdata$Global_active_power~subdata$datetime, type="l",
-     ylab="Global Active Power", xlab="")
+     ylab="Global Active Power", xlab="")                      # plot number 1
 
 plot(subdata$Voltage~subdata$datetime, type="l",
-     ylab="Voltage", xlab="datetime")
+     ylab="Voltage", xlab="datetime")                          # plot number 2
 
 plot(subdata$Sub_metering_1~subdata$datetime, type="l",
      ylab="Energy sub metering", xlab="")
@@ -29,8 +29,9 @@ points(subdata$Sub_metering_2~subdata$datetime, type="l",
 points(subdata$Sub_metering_3~subdata$datetime, type="l",
        ylab="Energy sub metering", xlab="", col="blue")
 legend("topright", lty=1, lwd=2, col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+                                                                  # plot number 3
 
 plot(subdata$Global_reactive_power~subdata$datetime, type="l",
-     ylab="Global_reactive_power", xlab="datetime")
+     ylab="Global_reactive_power", xlab="datetime")                # plot number 4
 
 dev.off()
